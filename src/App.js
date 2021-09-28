309931e9faafe9a926ff0d302ed8830d023bc97a
atul-gairola <atulgairola08@gmail.com>
2021-09-29 01:01:55 +0530

    feat: handle state + check for luck birthdate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({
+    birthdate: "",
+    luckyNumber: "",
+  });
+  const [result, setResult] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const getSumOfDigitsInBirthdate = (birthdate) => {
+    const birthdateArr = birthdate.replaceAll("-", "").split("");
+    return birthdateArr.reduce((total, cur) => {
+      return total + Number(cur);
+    }, 0);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setResult("");
+    
+    const { birthdate, luckyNumber } = data;
+    
+    if (!birthdate || !luckyNumber) {
+      setResult("Please fill both the fields");
+      return;
+    }
+
+    const sum = getSumOfDigitsInBirthdate(birthdate);
+    if (sum % luckyNumber === 0) {
+      setResult("Congratulations!! You have a lucky birthdate");
+    } else {
+      setResult("Your birthdate is not so lucky :(");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Is your birthday lucky?</h1>
       </header>
+      <main>
+        <form>
+          <input
+            type="date"
+            name="birthdate"
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="number"
+            name="luckyNumber"
+            min="0"
+            onChange={handleInputChange}
+            required
+          />
+          <button onClick={handleSubmit}>Is it?</button>
+        </form>
+        <div>{result && <p>{result}</p>}</div>
+      </main>
+      <footer>
+        <p>
+          Made with love by <a href="https://atulgairola.tech">Atul Gairola</a>
+        </p>
+      </footer>
     </div>
   );
 }
